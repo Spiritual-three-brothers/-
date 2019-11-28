229cf801fa578e22f106d82bd31eb0b2ba8f76ab
shen <weijia618@outlook.com>
2019-11-28 16:34:34 +0800

    2019.11.28

--- src/pages/classify/index.vue
+++ src/pages/classify/index.vue
@@ -1,17 +1,71 @@
 <template>
-  <div>
-    <div class="navbar">
-      <block v-for="(item,index) in tabs" :key="index">
-        <div :id="index" :class="{'navbar_item_on':activeIndex == index}" class="navbar_item" @click="tabClick">
-          <div class="navbar_title">{{item.name}}</div>
-        </div>
-      </block>
-      <div class="navbar_slider" :class="navbarSliderClass"></div>
-    </div>
-    <div>
-      <div :hidden="activeIndex != 0">选项一的内容</div>
-      <div :hidden="activeIndex != 1">选项二的内容</div>
-      <div :hidden="activeIndex != 2">选项三的内容</div>
-    </div>
-  </div>
+      <div class="contain">
+        <!-- 顶部横向bar，主类别 -->
+          <scroll-view scroll-x class="top">
+              <div class="tabbar" :class="{'tabbar-bottom':currentTab==index}" v-for="(item,index) in tabBar" :key="index" @click="clickTab(index)">
+                  {{item.title}}
+              </div>
+          </scroll-view>
+          <!-- 横向bar的内容 -->
+          <swiper :current="currentTab" @change="changeTab">
+              <swiper-item v-for="item in items" :key="item.id">
+               <div>
+                   {{item.content}}
+               </div>
+             </swiper-item>
+         </swiper>
+     </div>
 </template>
+ 
+<script>
+export default {
+  data () {
+    return {
+      tabBar: [
+        { 'title': '生鲜果蔬' },
+        { 'title': '百货便利' },
+        { 'title': '家用电器' },
+        { 'title': '其他分类' }
+      ],
+      currentTab: 0,
+      items: [
+        { 'content': '苹果,香蕉,菠萝,茄子' },
+        { 'content': '电视,电脑,冰箱,水壶' },
+        { 'content': '菠萝' },
+        { 'content': '茄子' }
+      ]
+    }
+  },
+  methods: {
+    clickTab (e) {
+      this.currentTab = e
+    },
+    changeTab (e) {
+      this.currentTab = e.mp.detail.current
+    }
+  }
+}
+</script>
+
+<style>
+  .top {
+     width: 100%;
+     text-align: center;
+     line-height: 42px;
+     white-space: nowrap;
+     position: relative;
+     background: #fff;
+   }
+   .tabbar {
+     width: 180rpx;
+     font-size: 14px;
+     height: 79rpx;
+     display: inline-block;
+     color: #000;
+   }
+   
+   .tabbar-bottom {
+     color: #FB4D53;
+     border-bottom: 2px solid #FB4D53;
+   }
+   </style>
---
+++ src/pages/detail/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="main_container">
+    <swiper
+      :indicator-dots='true'
+      indicator-color='#d8d8d8'
+      indicator-active-color = '#ffad36'
+      :autoplay='true'
+      :interval='3000'
+      :duration='1000'
+      :circular='true'
+    >
+      <a v-for='img in topSwipers' :key="img.id">
+        <swiper-item>
+          <img              
+            class='slide-image' 
+            mode='aspectFit' 
+            :src='img.outterImage'
+            />
+        </swiper-item>
+      </a>
+    </swiper>
+    <div class="weight">
+        {{curritem.weight}}
+    </div>
+    <div class="price">
+        ￥{{curritem.price}}
+    </div>
+    <div class="full_title">
+        {{curritem.full_title}}
+    </div> 
+  </div>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      topSwipers: [{
+        id: 1,
+        outterImage: '../../static/swiper/a1.jpg'
+      },
+      {
+        id: 2,
+        outterImage: '../../static/swiper/a2.png'
+      },
+      {
+        id: 3,
+        outterImage: '../../static/swiper/a3.png'
+      }],
+      curritem: {
+        item_id: '3122313',
+        price: '12.3',
+        full_title: '天水红富士霜打极品 果农自摘不外售 十一月西北专属 纯绿色水果',
+        weight: '500g'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+swiper{
+    width: 100%;
+    height: 335px;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.6);  
+}
+.price{
+    margin-left: 11px;
+    font-size: 18px;
+    margin-top: 47px;
+    color: #FB4D53;
+    font-family: SourceHanSansSC-bold;
+}
+.weight{
+    border-radius: 20%;
+    background: gainsboro;
+    float: right;
+    margin-right: 11px;
+    margin-top: 47px;
+    color: rgb(43, 23, 24);
+    font-family: SourceHanSansSC-bold;
+}
+</style>

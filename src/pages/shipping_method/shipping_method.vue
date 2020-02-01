5c979cb95694291161a33d469d25880b1ee4d2bc
Allen <376528106@qq.com>
2020-02-01 14:16:44 +0800

    订单

--- src/pages/shipping_method/shipping_method.vue
+++ src/pages/shipping_method/shipping_method.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="shipping-method">
-    <div class="shipping-option">
-      <span class="iconfont" style="color: #707070">&#xe659;</span>
+    <div class="shipping-option-wrapper">
+      <div class="shipping-option-banner">
+        <span class="iconfont" style="color: #707070">&#xe659;</span>
+        <span class="text">配送方式</span>
+      </div>
+
+
+      <div class="shipping-option">
+        <div class="choice" :class="{selected: selected === 1}" @click="seller">商家配送</div>
+        <div class="choice" :class="{selected: selected === 2}" @click="self">上门自提</div>
+      </div>
+      <div class="d4" :class="{seller: selected === 1, self: selected === 2}"/>
     </div>
+
+    <div class="test"></div>
+
+
+
   </div>
 </template>
 
@@ -10,7 +25,15 @@
   export default {
     data () {
       return {
-
+        selected: 1
+      }
+    },
+    methods: {
+      seller () {
+        this.selected = 1
+      },
+      self () {
+        this.selected = 2
       }
     }
   }

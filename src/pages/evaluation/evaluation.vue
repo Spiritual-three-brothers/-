52b57e15a3624d4ba980e0a83cc8089e558943b3
Vicissitidues <weijia618@outlook.com>
2020-02-14 18:00:43 +0800

    writeEvaluationModel

--- src/pages/evaluation/evaluation.vue
+++ src/pages/evaluation/evaluation.vue
@@ -9,13 +9,51 @@
         </div>
       </div>
       <div class="evaluate_area">
-        <textarea v-model="items.count" type="text" ref="count" placeholder="评价大于20元的商品超过十个字就有机会获得优惠券"></textarea>
-      </div>
-       <div class="statistic">
-          {{items.count.length}}/25
+        <textarea v-model="count" type="text" ref="count" placeholder="评价大于20元的商品超过十个字就有机会获得优惠券"></textarea>
+         <div class="input_listening">
+          {{count.length}}/140
         </div>
+      </div>
+      <div class="upload">
+        <p class="pick-title">添加图片</p>
+        <div class="weui-uploader__bd">
+          <div class="weui-uploader__files" id="uploaderFiles">
+            <block v-for="(item,index) in files" :key="index">
+              <div class="weui-uploader__file" @click="previewImage($event,index)" :id="item">
+                <image class="weui-uploader__img" :src="item" mode="aspectFit" />
+              </div>
+            </block>
+          </div>
+          <div class="weui-uploader__input-box" >
+            <div class="weui-uploader__input"  @click="chooseImage($event)">
+              <div class="camera">&#xe612;</div>
+              <div class="discribe">添加图片</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="send_style">
+        <div  :class="button" @click="selected()"></div>
+        <div class="discribe">匿名发布</div>
+      </div>
     </div>
-    <div class="rate"></div>
+    <div class="rate">
+      <div class="shop_service_comment">
+        <div class="icon">&#xe613;</div>
+        <div class="discribe">店铺服务评价</div>
+      </div>
+      <div class="service_container">
+        <div class="service_rate">
+          <div class="service_container" v-for="(single_rate, index_) in type" :key="single_rate.id">
+            <div class="discribe">{{single_rate}}</div>
+            <div class="star_container">
+              <div class="star" v-for="(item, index) in list" :key="item.id" :id="single_rate" :class="{changeBGC:changeColor >= index}" @click="whichStar($event,index)">★</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="submit"> 提交 </div>
+    </div>
   </div>
 </template>
 
@@ -24,17 +62,58 @@
 export default {
   data () {
     return {
+      type: ['商品符合度', '店铺服务态度', '商家配送速度', '配送员服务'],
       list: [0, 1, 2, 3, 4],
       changeColor: '',
-      stars: []
+      count: '',
+      stars: [],
+      button: 'button'
     }
   },
   mounted () {
   },
   methods: {
     whichStar (e, index) {
-      // console.log(e.currentTarget.getElementById('2'))
       this.changeColor = index
+    },
+    selected () {
+      if (this.button === 'button') {
+        this.button = 'selected'
+      } else {
+        this.button = 'button'
+      }
+    },
+    chooseImage (e) {
+      console.log('onclicked')
+      let i = 0
+      let that = this
+      let max = that.maxImg
+      let upLength
+      let imgFilePaths
+      wx.chooseImage({
+        count: max || 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success: function (res) {
+          let len = that.files.concat(res.tempFilePaths)
+          imgFilePaths = res.tempFilePaths
+          upLength = imgFilePaths.length
+          if (len.length > max) {
+            that.$mptoast('图片最多只能选择' + max)
+            return false
+          }
+          wx.showLoading({
+            title: '上传中...'
+          })
+          that.upLoad(imgFilePaths, i, upLength)
+        },
+        fail: function () {
+          console.log('fail')
+        },
+        complete: function () {
+          console.log('commplete')
+        }
+      })
     }
   }
 }
--- src/pages/unpaid/unpaid.vue
+++ src/pages/unpaid/unpaid.vue
@@ -1,41 +1,45 @@
 <template>
   <div class="main_container">
     <div class="order_state">
-      <div class="unpaid_alert">
-        <div class="icon iconfont">&#xe60f;</div>
-        <div class="order_cost">
-          <div class="unpaid">待支付</div>
-          <div class="cost">应付金额：￥23.6</div>
+      <div class="display_repair">
+        <div class="unpaid_alert">
+          <div class="icon iconfont">&#xe60f;</div>
+          <div class="order_cost">
+            <div class="unpaid">待支付</div>
+            <div class="cost">应付金额：￥23.6</div>
+          </div>
+          <div class="rest_time">支付剩余:23时59分45秒</div>
         </div>
-        <div class="rest_time">支付剩余:23时59分45秒</div>
-      </div>
-      <div class="user_info">
-        <div class="icon iconfont">&#xe702;</div>
-        <div class="user_detail">
-          <div class="tel">周诗宇 181****9221</div>
-          <div class="address">四川省成都市高新区泰和佳园蜂巢</div>
+        <div class="user_info">
+          <div class="icon iconfont">&#xe702;</div>
+          <div class="user_detail">
+            <div class="tel">周诗宇 181****9221</div>
+            <div class="address">四川省成都市高新区泰和佳园蜂巢</div>
+          </div>
+          <div class="white_space">修改</div>
         </div>
-        <div class="white_space"></div>
       </div>
     </div>
     <div class="order_content">
-      <div class="shop_title">
-        <div class="icon">&#xe703;</div>
-        <div class="shop_name">东百好果汁发哥旗舰店&#xe637;</div>
-        <div class="count_total">共计1件商品</div>
-      </div>
-      <div class="shop_inner">
-        <div class="goods_img">图片</div>
-        <div class="goods_detail">
-          <div class="item_name">天水红富士霜打极品 果农自摘不外售 十一月西...</div>
-          <div class="item_type">一斤糖心</div>
-          <div class="item">
-            <div class="price">￥36.8</div>
-            <div class="count">×1</div>
+      <div class="display_repair">
+        <div class="shop_title">
+          <div class="icon">&#xe703;</div>
+          <div class="shop_name">东百好果汁发哥旗舰店 &#xe637;</div>
+          <div class="count_total">共计1件商品</div>
+        </div>
+        <div class="shop_inner">
+          <div class="goods_img">图片</div>
+          <div class="goods_detail">
+            <div class="item_name">天水红富士霜打极品 果农自摘不外售 十一月西...</div>
+            <div class="item_type">一斤糖心</div>
+            <div class="item">
+              <div class="price">￥36.8</div>
+              <div class="count">×1</div>
+            </div>
           </div>
         </div>
+        <div class="connect_service">&#xe610; 联系客服</div>
       </div>
-      <div class="connect_service">&#xe610; 联系客服</div>
     </div>
     <div class="order_info">
       <div class="order_detail">
@@ -63,7 +67,7 @@
           <div class="type">商品金额</div><div class="cost">￥29.00</div>
         </div>
         <div class="settlement_container">
-          <div class="type">运费</div><div class="cost">+￥5.00</div>
+          <div class="type">运费</div><div class="cost">&#xe636; ￥5.00</div>
         </div>
         <div class="total">
           <div class="cost">￥34.00</div><div class="type">应付金额：</div>

7004c91440705344fa6a16724a54c101ec22e3d5
shen <weijia618@outlook.com>
2020-03-13 16:10:26 +0800

    shopPage

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -51,6 +51,7 @@
         </div>
         <!-- 测试页面入口 -->
         <div @click="test_enter" style="text-align:center">测试入口</div>
+        <div @click="test_enter2" style="text-align:center">测试入口</div>
         <!-- 促销页面 -->
         <div class="saling">
           <div class="title_contant">
@@ -218,7 +219,7 @@
         wx.navigateTo({url: '../unpaid/main'})
       },
       test_enter2 () {
-        wx.navigateTo({url: '../evaluation/main'})
+        wx.navigateTo({url: '../shop_page/main'})
       }
     },
 
---
+++ src/pages/shop_page/shop_page.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="this_container">
+    <div class="shop_page">
+      <div class="shop_title">
+        <div class="title_container">
+          <img class="shop_img" src="https://pic1.zhimg.com/v2-8c13c9f55882ff2bd9f0a3f03d32e0ec_1200x500.jpg">
+          <div class="shop_name">熹茶东百体验店ShiteaDB Store</div>
+          </div>
+        <div class="shop_info">
+          <div class="month_sail">
+            <div class="sail_title">月销量</div>
+            <div class="sail_count">10w</div>
+          </div>
+          <div class="month_sail">
+            <div class="sail_title">描述相符</div>
+            <div class="sail_count">10w</div>
+          </div>
+          <div class="month_sail">
+            <div class="sail_title">商家服务</div>
+            <div class="sail_count">10w</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        shops: []
+      }
+    },
+    created () {
+    },
+    methods: {
+      // back2menu () {
+      //   wx.switchTab({url: '../index/main'})
+      // }
+    }
+  }
+</script>
+
+
+<style lang="sass">
+  @import "shop_page"
+</style>

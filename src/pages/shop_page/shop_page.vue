01073f1c2c5b92ef768a6e50ad0fdfd299b5791e
shen <weijia618@outlook.com>
2020-03-16 16:35:13 +0800

    finShopPage

--- src/pages/shop_page/shop_page.vue
+++ src/pages/shop_page/shop_page.vue
@@ -20,6 +20,81 @@
             <div class="sail_count">10w</div>
           </div>
         </div>
+        <div class="switch_bar">
+          <div class="all_selection">
+            
+                <div class="top">
+                  <scroll-view  scroll-x class="tab-bar-container">
+                    <div class="tabbar" :class="{'tabbar-bottom':currentTab===index}" v-for="(item,index) in tabBar" :key="index"
+                        @click="clickTab(index)">
+                      {{item.title}}
+                    </div>
+                  </scroll-view>
+
+                  <div class="swiper-container">
+                    <scroll-view scroll-x class="sub-category">
+                      <span @click="clickSub(_index)" :class="{'selected': selectedSub === _index}" class="sub-category-item"
+                            v-for="(sub,_index) in tabBar[currentTab].subcategory" :key="sub.id">{{sub.title}}</span>
+                    </scroll-view>
+                    <div class="item-list">
+
+                      <div class="item-content" @click="navigate">
+                        <div class="item-container">
+                          <div class="image-container">
+                            <img src="../../../static/item/osm.jpg">
+                          </div>
+                          <div class="item_info_container">
+                            <div class="item-title">
+                              欧诗漫OSM美白化妆品套装 营养美肤晶彩无暇补水保湿护肤品
+                            </div>
+                            <div class="priceAndSell">
+                              <div class="item-price">
+                                <span style="font-size: 12px">￥</span>25.53
+                              </div>
+                              <div class="item-comment">
+                                13万条评价
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                       <div class="item-container">
+                          <div class="image-container">
+                            <img src="../../../static/item/osm.jpg">
+                          </div>
+                          <div class="item_info_container">
+                            <div class="item-title">
+                              欧诗漫OSM美白化妆品套装 营养美肤晶彩无暇补水保湿护肤品
+                            </div>
+                            <div class="priceAndSell">
+                              <div class="item-price">
+                                <span style="font-size: 12px">￥</span>25.53
+                              </div>
+                              <div class="item-comment">
+                                13万条评价
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom_bar">
+      <div class="bottom_container">
+        <div class="bottom_selection">
+          <div class="bottom_icon">&#xe617;</div>
+          <div class="selection_title">商品分类</div>
+        </div>
+        <div class="bottom_selection">
+          <div class="bottom_icon">&#xe616;</div>
+          <div class="selection_title">联系客服</div>
+          </div>
       </div>
     </div>
   </div>
@@ -30,19 +105,65 @@
   export default {
     data () {
       return {
-        shops: []
+        tabBar: [
+          {
+            id: 1,
+            title: '全部',
+            subcategory: [
+              {id: 1, title: '默认'},
+              {id: 2, title: '销量'},
+              {id: 3, title: '精品'},
+              {id: 4, title: '特惠'}
+            ]
+          },
+          {
+            id: 2,
+            title: '劲爆活动',
+            subcategory: [
+              {id: 1, title: '饮料'},
+              {id: 2, title: '零食'},
+              {id: 3, title: '日用品'},
+              {id: 4, title: '乳制品'},
+              {id: 5, title: '生活用品'}
+            ]
+          }
+        ],
+        currentTab: 0,
+        selectedSub: 0,
+        content: [
+          {
+            mainCateId: 1,
+            subCateGory: 1,
+            itemList: [
+              {
+                itemId: 1,
+                itemName: '天水红富士霜打极品 果农自摘不外售 十一月',
+                itemImage: ''
+              }
+            ]
+          }
+        ]
       }
     },
-    created () {
+    mounted () {
     },
     methods: {
-      // back2menu () {
-      //   wx.switchTab({url: '../index/main'})
-      // }
+      clickTab (e) {
+        this.currentTab = e
+      },
+      changeTab (e) {
+        this.currentTab = e.mp.detail.current
+        console.log(this.currentTab)
+      },
+      clickSub (idx) {
+        this.selectedSub = idx
+      },
+      navigate () {
+        wx.navigateTo({url: '../detail/main'})
+      }
     }
   }
 </script>
-
 
 <style lang="sass">
   @import "shop_page"
